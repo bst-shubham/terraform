b1481395a8f8cf49a9cf582c7807e377f343a714
bst-shubham <96282740+bst-shubham@users.noreply.github.com>
2021-12-28 17:27:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,13 +11,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_subnet" "public" {
-    count = "${length(var.availability_zones)}"
-    vpc_id = "vpc-0168fff8e4049b41d"
-    cidr_block = "${cidrsubnet(var.cidr_block, 8, count.index)}"
-    map_public_ip_on_launch = true
-    tags = {
-        "Name = prod-extended-subnet- ${element(var.availability_zones, count.index)}
-    }
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }

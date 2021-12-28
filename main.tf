994ead0e2fca44995f0801ec3feaad337218608d
bst-shubham <96282740+bst-shubham@users.noreply.github.com>
2021-12-28 15:56:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "dev-now-gg-terraform-up-and-running-state-shubham"
+    key = "main"
+    region = "ap-south-1"
+    dynamodb_table = "S3-Terraform-Statefile-Table"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }

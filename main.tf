7caafbeccc1d03c0cadafc2c6ef62d3b61f1154c
bst-shubham <96282740+bst-shubham@users.noreply.github.com>
2022-01-02 14:04:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,44 @@ terraform {
     dynamodb_table = "S3-Terraform-Statefile-Table"
   }
 }
-
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_vpc" "NowggVpc" {
+    cidr_block = var.VpcCidrBlock
+    instance_tenancy = var.InstanceTenancy
+    enable_dns_support = var.EnableDnsSupport
+    enable_dns_hostnames = var.DnsHostName
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_internet_gateway" "Internet_Gateway" {
+    vpc_id = aws_vpc.NowggVpc.id
+    tags = {
+        Name = "Nowgg Internet Gateway"
+    }
+
 }
+
+resource "aws_route" "Nowggdefaultroute" {
+    route_table_id = aws_vpc.NowggVpc.default_route_table_id
+    destination_cidr_block = var.destinationcidrblock
+    gateway_id = aws_internet_gateway.Internet_Gateway.id
+}
+
+resource "aws_subnet" "public" {
+    count = "${length(var.availability_zones)}"
+    vpc_id = aws_vpc.NowggVpc.id
+    cidr_block = "${cidrsubnet(var.cidr_block_subnet, 8, count.index)}"
+    map_public_ip_on_launch = true
+    tags = {
+        "Name" = "prod-extended-subnet- ${element(var.availability_zones, count.index)}"
+    }
+}
+
+resource "aws_route_table_association" "subnet_route_association" {
+    route_table_id = aws_vpc.NowggVpc.default_route_table_id
+    count = "${length(var.availability_zones)}"
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+}
+
+
+
+
+
